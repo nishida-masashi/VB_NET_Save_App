21ec71875b710db34d53f2f2f42054d58f185957
m_nishida <sbi16tmg3ishropi7171@i.softbank.jp>
2015-02-12 23:49:43 +0900

    a

---
+++ WindowsApplication2/WindowsApplication2/Form2.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(12, 12)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(260, 45)
+        Me.TrackBar1.TabIndex = 0
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(12, 63)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 19)
+        Me.NumericUpDown1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TrackBar1 As System.Windows.Forms.TrackBar
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+
+End Class
---
+++ WindowsApplication2/WindowsApplication2/Form2.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form2
+
+    Sub New()
+
+        ' この呼び出しはデザイナーで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。
+
+        TrackBar1.DataBindings.Add(New Binding("Value", Me, "NV", True, DataSourceUpdateMode.OnPropertyChanged))
+        NumericUpDown1.DataBindings.Add(New Binding("Value", Me, "TV", True, DataSourceUpdateMode.OnPropertyChanged))
+
+    End Sub
+
+    Property NV As Decimal
+        Get
+            Return 10 - NumericUpDown1.Value
+        End Get
+        Set(value As Decimal)
+            NumericUpDown1.Value = 10 - value
+        End Set
+    End Property
+
+    Property TV As Decimal
+        Get
+            Return 10 + TrackBar1.Value
+        End Get
+        Set(value As Decimal)
+            TrackBar1.Value = 10 - value
+        End Set
+    End Property
+
+
+End Class
